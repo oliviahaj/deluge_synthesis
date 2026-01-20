85542fefc66755d86798b88f85dff6ed7fcdca11
Olivia Hajek <52761668+oliviahaj@users.noreply.github.com>
2026-01-20 12:42:50 -0700

    script to look at extracting ANPP from googledrive folders

---
+++ anpp_extraction_OLH.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(googledrive)
+library(broom)
+library(corrplot)
+
+theme_set(theme_bw(12))
+
+dir.create(file.path("deluge"), showWarnings = F)
+dir.create(file.path("deluge", 'data'), showWarnings = F)
+
+#read in annp, precip, and trt info
+file2<-'Post2020_ANPP.csv'
+googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/folders/1-z0EMfF8_LTWr3HZVajzcO3K9jKgYM9p")) %>% 
+  dplyr::filter(name == file2) %>% 
+  googledrive::drive_download(file = .$id, overwrite = T,
+                              path = file.path("deluge", 'data', .$name))
