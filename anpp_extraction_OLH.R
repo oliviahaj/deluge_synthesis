d3ecb94aa42befc4bf0c10fd9afad1dd3f52e21e
Olivia Hajek <52761668+oliviahaj@users.noreply.github.com>
2026-01-23 07:27:40 -0700

    updating structure of R script

--- anpp_extraction_OLH.R
+++ anpp_extraction_OLH.R
@@ -8,9 +8,13 @@ theme_set(theme_bw(12))
 dir.create(file.path("deluge"), showWarnings = F)
 dir.create(file.path("deluge", 'data'), showWarnings = F)
 
+
+## Post and Knapp 2020
 #read in annp, precip, and trt info
 file2<-'Post2020_ANPP.csv'
 googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/folders/1-z0EMfF8_LTWr3HZVajzcO3K9jKgYM9p")) %>% 
   dplyr::filter(name == file2) %>% 
   googledrive::drive_download(file = .$id, overwrite = T,
                               path = file.path("deluge", 'data', .$name))
+
+## Calculate s
