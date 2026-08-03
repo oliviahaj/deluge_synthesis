ffd4384ce66e5d343f7579617af91db15322c87f
Olivia Hajek <52761668+oliviahaj@users.noreply.github.com>
2026-08-03 12:50:34 -0600

    inventory figure

--- data_inventory_graph.R
+++ data_inventory_graph.R
@@ -37,6 +37,71 @@ p_top <- ggplot(df_counts, aes(x = variable, y = n)) +
         axis.ticks.x = element_blank()) +
   xlab("") + ylab("# Studies")
 
+
+
+# Updated ESA figure
+unique(data_long$Study)
+combined_colors<-c(
+  # Post2020
+  "Post and Knapp 2020"       = "#332288",
+  
+  # Post2021
+  "Post and Knapp 2021"       = "#88ccee",
+
+  # Hoover
+  "Hoover et al. 2022"     = "#999933",
+  
+  # Condon_SL
+  "Condon and Knapp Ecosphere"     = "#117733",
+
+  # Linabury
+  "Linabury CHANGE plots"     = "#cc6677",
+  
+  # CCE
+  "Siggers et al. CEE"     = "#ddcc77",
+
+  # Hajek
+  "Hajek and Knapp 2024" = "#44AA99",
+
+  # Tooley - 
+  "Tooley DRE" = "#882255"
+)
+
+
+data_long.2 <- data_long %>%
+  filter(value == 1)
+unique(data_long.2$variable)
+
+data_long.2 <- data_long.2 %>%
+  mutate(variable = recode_values(variable,
+                               "ANPP"              ~ "ANPP",
+                               "funct_groups"      ~ "ANPP - Func. Gp.",
+                               "BNPP"              ~ "BNPP",
+                               "Soil_moisture"     ~ "20cm Soil Moisture",
+                               "Soil_respiration"  ~ "Soil Respiration",
+                               "Canopy_greenness"  ~ "Canopy Greenness",
+                               "Water_potential"   ~ "Water Potential",
+                               "Sentek"            ~ "Deep Soil Moisture",
+                               "PRS"               ~ "Nutrient Probes"
+  )) %>%
+  mutate(variable = factor(variable, levels = c(
+    "Water Potential", "Nutrient Probes", "Soil Respiration", "Canopy Greenness",
+    "Deep Soil Moisture", "20cm Soil Moisture", "BNPP", "ANPP - Func. Gp.",
+    "ANPP"
+  )))
+
+
+esa.det <- ggplot(data_long.2, aes(x = variable, fill = Study)) +
+  geom_bar() +
+  coord_flip()+
+  scale_fill_manual(values=combined_colors)+
+  labs(x = "Variable", y = "Number of studies", fill = "Study") +
+  theme_minimal(base_size = 16)
+
+ggsave("/Users/olhajek/Desktop/deluge_synthesis/Deluge_synthesis/deluge/figures/data_inventory.pdf", 
+       plot = esa.det, width = 12, height = 6, dpi = 600)
+
+
 # main tile plot
 p_main <- ggplot(data_long, aes(x = variable, y = Study, fill = factor(value))) +
   geom_tile(color = "grey80") +
